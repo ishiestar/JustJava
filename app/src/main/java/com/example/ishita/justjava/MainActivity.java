4e5297d58f97f11fc8291acd26eab67e6754a600
Ishita <ishiestar@gmail.com>
2015-12-24 16:14:44 +0530

    Second App

--- app/src/main/java/com/example/ishita/justjava/MainActivity.java
+++ app/src/main/java/com/example/ishita/justjava/MainActivity.java
@@ -1,52 +1,67 @@
 package com.example.ishita.justjava;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.widget.TextView;
 
+import java.text.NumberFormat;
+
+/**
+ * This app displays an order form to order coffee.
+ */
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+    /**
+     * This method is called when the order button is clicked.
+     */
+    public void submitOrder(View view) {
+        //display(2);
+        displayPrice(5);
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+    /**
+     * This method displays the given quantity value on the screen.
+     */
+    /*private void display(int number) {
+        TextView quantityTextView = (TextView) findViewById(
+                R.id.quantity_text_view);
+        quantityTextView.setText("" + number);
+    }*/
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
+    /**
+     * This is the method that increases the quantity when the "+" button is pressed.
+     */
+    public void increment(View view) {
+        TextView quantityTextView = (TextView) findViewById(
+                R.id.quantity_text_view);
+        int value=Integer.parseInt((String)quantityTextView.getText());
+        value++;
+        quantityTextView.setText("" + value);
+    }
 
-        return super.onOptionsItemSelected(item);
+    public void decrement(View view) {
+        TextView quantityTextView = (TextView) findViewById(
+                R.id.quantity_text_view);
+        int value=Integer.parseInt((String)quantityTextView.getText());
+        if(value>0) value--;
+        quantityTextView.setText("" + value);
+    }
+
+    /**
+     * This method displays the given price on the screen.
+     */
+    private void displayPrice(int number) {
+        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
+        TextView quantityTextView = (TextView) findViewById(
+                R.id.quantity_text_view);
+        number=number*(Integer.parseInt((String)quantityTextView.getText()));
+        priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
     }
 }
